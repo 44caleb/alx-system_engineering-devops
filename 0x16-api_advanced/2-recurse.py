b29c8a6b198f2ee4662c15d81a78d6fa908dfcbe
44caleb <calebadinfono2@gmail.com>
2023-11-08 12:13:42 +0000

    new recurse

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,23 +1,34 @@
 #!/usr/bin/python3
-"""recursively gets hot topics in a subreddit"""
+"recursively searches for hot topics in a subreddit"""
 
 
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None):
-    user_agent = {'User-agent': 'Mozilla/5.0'}
-    sub = requests.get('http://www.reddit.com/r/{}/hot.json?after={}'
-                       .format(subreddit, after), headers=user_agent)
+def recurse(subreddit, hot_list=[]):
+    """recursively gets hot topics in a subreddit"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    res = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
+    if res.status_code != 200:
+        return None
+    result = res.json()
+    if len(result["data"]["children"]) == 0:
+        return None
+    after = result["data"]["after"]
+    hot_list = rec_function(hot_list, after, url)
+    return hot_list
 
-    try:
-        sub = sub.json().get('data')
-        after = sub.get('after')
-        sub = sub.get('children')
-        for obj in sub:
-            hot_list.append(obj['data'].get('title'))
-        if after is not None:
-            recurse(subreddit, hot_list, after)
-        return(hot_list)
-    except:
-        return(None)
+
+def rec_function(hot_list, after, url):
+    """recursive function"""
+    if not after:
+        return hot_list
+    else:
+        res = requests.get(url, params={"after": after},
+                           headers={"User-Agent": "Mozilla/5.0"})
+        result = res.json()
+        new_after = result["data"]["after"]
+        for child in result["data"]["children"]:
+            title = child["data"]["title"]
+            hot_list.append(title)
+        return rec_function(hot_list, new_after, url)
